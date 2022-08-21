901d3556afde1e5ee73b30b4f8f04d11b4bd0b65
Henrik Strand <strand@t-k-l.dk>
2022-08-21 12:50:24 +0200

    Update set-hostname.sh

--- set-hostname.sh
+++ set-hostname.sh
@@ -10,7 +10,4 @@ sudo echo "Awesome!"
 sudo hostnamectl set-hostname $hostname
 sudo sed -i "s/vanilla-debian/$hostname/g" /etc/hosts
 
-echo "Please remove set-hostname.sh in homedir"
 echo "Remember to reboot system"
-
-sed '/set-hostname.sh/d' .bashrc
