c7c3a010127c140cf0356e9cb15bcc7eaef8e713
Henrik Strand <strand@t-k-l.dk>
2022-08-21 12:15:35 +0200

    Create set-hostname.sh

---
+++ set-hostname.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Input hostname
+echo "Type hostname: "
+read hostname
+
+echo "Type sudo password: "
+sudo echo "Awesome!"
+
+sudo hostnamectl set-hostname $hostname
+sudo sed -i "s/vanilla-debian/$hostname/g" /etc/hosts
+
+echo "Please remove set-hostname.sh in homedir"
+echo "Remember to reboot system"
